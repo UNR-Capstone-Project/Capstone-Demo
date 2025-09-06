3437db6541f62c8e09167de73fda282f4886c0d0
Hyphenix <itsvictordang@gmail.com>
2025-09-06 14:19:04 -0700

    Implemented Basic Player Move and Jump + Temp Asset

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,14 +3,42 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpHeight = 2f;
+    [SerializeField] private float gravity = -25f;
+    private CharacterController controller;
+    private Vector3 moveInput;
+    private Vector3 velocity;
+
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+        Debug.Log($"Move input: {moveInput}");
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        Debug.Log($"Jumping {context.performed} - Is Grounded: {controller.isGrounded}");
+        if (context.performed && controller.isGrounded)
+        {
+            Debug.Log("Player is now jumping.");
+            velocity.y = Mathf.Sqrt(jumpHeight * -1.8f * gravity);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 move = new Vector3(moveInput.x, 0, moveInput.y);
+        controller.Move(move * moveSpeed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 }
