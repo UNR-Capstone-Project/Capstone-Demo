4d8a0ffade40b3a0b7dd2c4cd0af8c829a193362
andy-wittig <157990692+andy-wittig@users.noreply.github.com>
2025-09-09 02:03:26 -0700

    Visual overhaul

--- Assets/Scripts/Objects/Billboard.cs
+++ Assets/Scripts/Objects/Billboard.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class Billboard : MonoBehaviour
 {
+    public bool lockZRotation = false;
+
     private Transform cameraPos;
 
     void Start()
@@ -18,6 +20,15 @@ public class Billboard : MonoBehaviour
 
     void LateUpdate()
     {
-        transform.LookAt(cameraPos.position);
+        if (lockZRotation)
+        {
+            float cameraXRotation = cameraPos.rotation.eulerAngles.x;
+            float cameraYRotation = cameraPos.rotation.eulerAngles.y;
+            transform.rotation = Quaternion.Euler(cameraXRotation, cameraYRotation, 0f);
+        }
+        else
+        {
+            transform.LookAt(transform.position + (transform.position - cameraPos.position)); //flip z-axis to face camera
+        }
     }
 }
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -49,10 +49,8 @@ public class Player : MonoBehaviour
         if (Mathf.Abs(input.x) > 0)
         {
             Vector3 currentScale = playerSprite.transform.localScale;
-            currentScale.x = Mathf.Sign(input.x) * Mathf.Abs(currentScale.x);
+            currentScale.x = -Mathf.Sign(input.x) * Mathf.Abs(currentScale.x);
             playerSprite.transform.localScale = currentScale;
         }
-
-        //Control Sprite
     }
 }
